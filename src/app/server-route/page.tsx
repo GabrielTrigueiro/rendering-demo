e6ca01fb9a857b13bb56fbc937c2a8ccb46ba778
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2024-12-23 10:00:23 -0300

    usando client components em server componentes

--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,12 +1,14 @@
+import ImageSlider from "@/components/ImageSlide";
 import { serverSideFunction } from "@/utils/server-utils";
 
 export default function ServerRoutePage() {
-  console.log("Server route rendered");
   const result = serverSideFunction();
+  console.log("Server route rendered");
   return (
     <>
       <h1>ServerRoutePage</h1>
       <p>{result}</p>
+      <ImageSlider />
     </>
   );
 }
--- src/components/ImageSlide.tsx
+++ src/components/ImageSlide.tsx
@@ -6,7 +6,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export default function ClientRoutePage() {
+export default function ImageSlider() {
   const settings = {
     dots: true,
   };
