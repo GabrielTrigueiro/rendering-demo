b1bffd1dbfc2a9246f2e3b8e8ce3699669befe08
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2024-12-23 09:36:25 -0300

    criando funcoes que só podem ser usadas por server components

---
+++ src/app/client-route/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { serverSideFunction } from "@/utils/server-utils";
+
+export default function ClientRoutePage() {
+  console.log("Client route rendered");
+  return <h1>ClientRoutePage</h1>;
+}
